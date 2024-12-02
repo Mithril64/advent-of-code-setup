76e9409dceac32ce2e23f6874cb773de2f46ea0f
Timofei Gerasimov <timofei.gerasimov@student.nhlstenden.com>
2024-12-02 23:18:23 +0100

    updated the input curl to cover more scenarios

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ ! -f .env ]; then
     echo ".env file not found! Please create it with your session token."
     exit 1
@@ -22,13 +24,39 @@ fi
 BASE_URL="https://adventofcode.com/$AOC_YEAR/day"
 TEMPLATE=$(cat template.cpp)
 
-CURRENT_DAY=$(date -u --date="TZ=\"America/New_York\" now" +%d | sed 's/^0//')
+CURRENT_DAY=$(TZ=America/New_York date +%d | sed 's/^0//')
 
 if [ "$AOC_YEAR" -lt "$CURRENT_YEAR" ]; then
     MAX_DAY=25
 else
     MAX_DAY=$CURRENT_DAY
 fi
+
+fetch_input() {
+    local day=$1
+    local input_file=$2
+    local input_url=$3
+    local retries=5
+    local delay=30
+
+    while (( retries > 0 )); do
+        echo "Fetching input for day $day..."
+        curl -s -o "$input_file" --cookie "session=$SESSION" "$input_url"
+        
+        if [ -f "$input_file" ] && ! grep -q "500 Internal Server Error" "$input_file"; then
+            echo "Successfully fetched input for day $day."
+            return 0
+        else
+            echo "Failed to fetch input for day $day. Retrying in $delay seconds..."
+            rm -f "$input_file" 
+            sleep $delay
+            ((retries--))
+        fi
+    done
+
+    echo "Failed to fetch input for day $day after multiple attempts."
+    return 1
+}
 
 for day in $(seq 1 "$MAX_DAY"); do
     echo "Setting up for day $day of $AOC_YEAR..."
@@ -50,8 +78,7 @@ for day in $(seq 1 "$MAX_DAY"); do
     INPUT_URL="${BASE_URL}/${day}/input"
     INPUT_FILE="day${day}/input.txt"
     if [ ! -f "$INPUT_FILE" ]; then
-        echo "Fetching input for day $day..."
-        curl -o "$INPUT_FILE" --cookie "session=$SESSION" "$INPUT_URL"
+        fetch_input "$day" "$INPUT_FILE" "$INPUT_URL"
     else
         echo "Input file for day $day already exists."
     fi
